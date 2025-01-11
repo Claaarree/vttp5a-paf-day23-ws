43d1ee2178af194a3fb9b73606e51682420680d4
Claaarree <clare.fh@hotmail.com>
2025-01-11 10:42:29 +0800

    added comments on repo

--- src/main/java/vttp5a_paf/day23ws/repository/OrderRepository.java
+++ src/main/java/vttp5a_paf/day23ws/repository/OrderRepository.java
@@ -20,12 +20,28 @@ public class OrderRepository {
 
         rs.next();
         try {
+            
             return Optional.of(Order.toOrder(rs));
         } catch (Exception e) {
             // why does the query gimme a null row when there's nothing?
             // because the aggregate sum() is used in the query..
             // if don't use then is fine and can do as per normal...
             // not really sure why tho...
+
+            // no cannot do as per normal!
+            // need to use subquery..
+            // select id, sum(total_price) total_sum, sum(cost_price) cost_sum from 
+            // (select o.id, o.order_date, o.customer_id,
+            //     (d.quantity * d.unit_price * (1 - d.discount)) as total_price,
+            //     (d.quantity * p.standard_cost) as cost_price
+            //     from orders o
+            //     join order_details d on o.id = d.order_id
+            //     join products p on d.product_id = p.id
+            //     where o.id = 44
+            //     order by cost_price desc
+            //     ) as t
+            //     group by id;
+
             return Optional.empty();
         }
     }
